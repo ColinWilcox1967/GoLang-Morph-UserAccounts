a7b776df33cf2067a588810a09f0b600d6bf0c20
Colin Wilcox <colinwilcox1967@gmail.com>
2020-10-23 10:58:33 +0100

    First commit for GitLab main project
    General tidy up
    Added dumping of user accounts file
    added Type to action result

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
+//
+// User Account Server
+//
+// (c) 2020 Morph Inc.
+//
+
+//TO DO : Session token handling
+
 package main
 
 import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"golang.org/x/crypto/bcrypt"
+	"strings"
+	//	"github.com/gorilla/securecookie"
 
 	simplelogging "github.com/colinwilcox1967/golangsimplelogging"
 	"github.com/gorilla/mux"
 )
 
 const (
-	DEFAULT_LOG_FILE string = "LOGFILE.TXT"
-	DEFAULT_PORT     string = "8080"
+	DEFAULT_LOG_FILE      string = "LOGFILE.TXT"
+	DEFAULT_PORT          string = "8080"
+	DEFAULT_ACCOUNTS_FILE string = "USERS.DAT"
+	SERVER_VERSION        string = "v0.1"
+)
+
+const (
+	ACTION_LOGIN               int = 1
+	ACTION_LOGOUT              int = 2
+	ACTION_ADD_USER_ACCOUNT    int = 3
+	ACTION_EDIT_USER_ACCOUNT   int = 4
+	ACTION_DELETE_USER_ACCOUNT int = 5
 )
 
 type UserAccountRecord struct {
@@ -31,16 +51,21 @@ type UserAccountRecord struct {
 	Date     string `json:"date"`
 }
 
-type Result struct {
+type ActionResult struct {
+	Type    int    `json:"type"`
 	Message string `json:"msg"`
 	Value   bool   `json:"value"`
 	Token   string `json:"token"`
 }
 
+// cookie handling
+//var cookieHandler = securecookie.New(securecookie.GenerateRandomKey(64),securecookie.GenerateRandomKey(32))
+
 var (
-	httpPort int
-	logfile  string
-	helpFlag *bool
+	httpPort     int
+	logfile      string
+	accountsFile string
+	helpFlag     *bool
 
 	userAccounts []UserAccountRecord
 )
@@ -57,9 +82,58 @@ func addUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(reqBody, &newAccount)
 
+	// hash the password before writing
+	_, newAccount.Password = generateHashFromPassword([]byte(newAccount.Password)) // ?? is this needed?
+
+	recordsRead := readUserAccountsFile(accountsFile)
+
 	userAccounts = append(userAccounts, newAccount)
 
+	recordsWritten := writeUserAccountsFile(accountsFile)
+
+	var result ActionResult
+
+	result.Type = ACTION_ADD_USER_ACCOUNT
+	result.Token = ""
+	if recordsWritten == recordsRead+1 {
+		msg := fmt.Sprintf("Added user account %s (username:'%s'\n", newAccount.Id, newAccount.Username)
+		simplelogging.LogMessage(msg, simplelogging.LOG_INFO)
+
+		result.Value = true
+		result.Message = "New user account created"
+	} else {
+		result.Value = false
+		result.Message = "Failed to add new user account"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(newAccount)
+
+}
+
+func editUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
+	simplelogging.LogMessage("Hit 'editUserAccountEndpoint'", simplelogging.LOG_INFO)
+	//	vars := mux.Vars(r)
+	//	id := vars["id"]
+
+	recordsRead := readUserAccountsFile(accountsFile)
+
+	// TO DO Update
+
+	recordsWritten := writeUserAccountsFile(accountsFile)
+
+	var result ActionResult
+	result.Type = ACTION_EDIT_USER_ACCOUNT
+	result.Token = ""
+	if recordsRead == recordsWritten {
+		result.Value = true
+		result.Message = "User account updated."
+	} else {
+		result.Value = false
+		result.Message = "Failed to update user account."
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
 }
 
 func deleteUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -69,25 +143,47 @@ func deleteUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
+	recordsRead := readUserAccountsFile(accountsFile)
+
 	for index, account := range userAccounts {
 
 		if string(account.Id) == id {
 			userAccounts = append(userAccounts[:index], userAccounts[index+1:]...)
+
+			msg := fmt.Sprintf("Removed user account %s (username:'%s'\n", id, account.Username)
+			simplelogging.LogMessage(msg, simplelogging.LOG_INFO)
+		}
+	}
+	recordsWritten := writeUserAccountsFile(accountsFile)
+
+	var result ActionResult
+
+	result.Type = ACTION_DELETE_USER_ACCOUNT
+	result.Token = ""
+	if recordsWritten == recordsRead-1 {
+		result.Value = true
+		result.Message = "User account deleted"
+	} else {
+		result.Value = false
+		result.Message = "Failed to delete user account"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+
+}
+
+func findUserAccountByUsername(username string) (bool, int) {
+
+	simplelogging.LogMessage("Hit 'findUserAccountByUsername'", simplelogging.LOG_INFO)
+
+	for index, value := range userAccounts {
+		if strings.ToUpper(value.Username) == strings.ToUpper(username) {
+			return true, index
 		}
 	}
 
-}
-
-func findUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	simplelogging.LogMessage("Hit 'findUserAccountEndpoint'", simplelogging.LOG_INFO)
-
-	//    vars := mux.Vars(r)
-	//    id := vars["id"]
-
-}
-
-func dummyPageEndpoint(w http.ResponseWriter, r *http.Request) {
+	return false, -1 // username not found
 }
 
 func loginUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -98,48 +194,57 @@ func loginUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
 	username := vars["username"]
 	password := vars["password"]
 
-	var result Result
-		
+	var result ActionResult
+
 	if username != "" && password != "" {
-		err, hashedPassword := generateHashFromPassword ([]byte(password))
+		err, hashedPassword := generateHashFromPassword([]byte(password))
 
 		if err == nil {
 
-			actualPassword := "" // TODO: load for accounts file
+			// load into memory and search for matching username
+			_ = readUserAccountsFile(accountsFile)
+			foundUser, index := findUserAccountByUsername(username)
+			if foundUser {
+				actualPassword := userAccounts[index].Password
 
-			if comparePasswordAgainstHash (actualPassword, []byte(hashedPassword)) {
-				result = createLoginAttemptResult (true, "Login Successfull")
+				if comparePasswordAgainstHash(actualPassword, []byte(hashedPassword)) {
+					result = createLoginAttemptResult(true, "Login Successfull")
 
-				// TODO: embedd a session token here
-
+					//			token := GetSessionToken(r)
+					//			setSession(token, r)
+				} else {
+					result = createLoginAttemptResult(false, "Login Failure - Incorrect Credentials")
+				}
 			} else {
-				result = createLoginAttemptResult (false, "Login Failure - Credential Mismatch")
+				result = createLoginAttemptResult(false, "Login Failure - User Not Found")
 			}
-		
 		} else {
-			result = createLoginAttemptResult (false, "Login Failure - Password Problem")
+			result = createLoginAttemptResult(false, "Login Failure - Credential Mismatch")
 		}
+
 	} else {
-		result = createLoginAttemptResult (false, "Login Failure - No Credentials Supplied")
+		result = createLoginAttemptResult(false, "Login Failure - No Credentials Supplied")
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result)
 }
 
-func createLoginAttemptResult (state bool, msg string) Result {
-	var loginResult Result
+func createLoginAttemptResult(state bool, msg string) ActionResult {
+	var result ActionResult
 
-	loginResult.Message = msg
-	loginResult.Value = state
-	loginResult.Token = ""
-	
-	return loginResult
+	result.Type = ACTION_LOGIN
+	result.Message = msg
+	result.Value = state
+	result.Token = ""
+
+	return result
 }
 
 func logoutUserAccountEndpoint(w http.ResponseWriter, r *http.Request) {
 	simplelogging.LogMessage("Hit 'LogoutUserAccountEndpoint'", simplelogging.LOG_INFO)
 
+	//	clearSession(r)
 }
 
 func handleRequests() {
@@ -147,10 +252,10 @@ func handleRequests() {
 	gmuxRouter := mux.NewRouter().StrictSlash(true)
 
 	gmuxRouter.HandleFunc("/login/{username}/{password}", loginUserAccountEndpoint)
-	gmuxRouter.HandleFunc("/logout", logoutUserAccountEndpoint).Methods("POST")
+	gmuxRouter.HandleFunc("/logout", logoutUserAccountEndpoint)
 	gmuxRouter.HandleFunc("/add", addUserAccountEndpoint).Methods("POST")
 	gmuxRouter.HandleFunc("/delete", deleteUserAccountEndpoint).Methods("DELETE")
-	gmuxRouter.HandleFunc("/find", findUserAccountEndpoint).Methods("GET")
+	gmuxRouter.HandleFunc("/edit", editUserAccountEndpoint).Methods("UPDATE")
 
 	portStr := fmt.Sprintf(":%d", httpPort)
 
@@ -178,7 +283,15 @@ func readUserAccountsFile(accountsFile string) int {
 	}
 }
 
-func writeAllUserAccounts() {
+func writeUserAccountsFile(filename string) int {
+	file, _ := json.Marshal(userAccounts)
+
+	err := ioutil.WriteFile(filename, file, 0644)
+	if err == nil {
+		return len(userAccounts)
+	}
+
+	return 0
 
 }
 
@@ -187,6 +300,7 @@ func handleCommandLineParameters() (int, error) {
 	var str string
 	flag.StringVar(&str, "port", DEFAULT_PORT, "Port on which the user account server will run.")
 	flag.StringVar(&logfile, "logfile", DEFAULT_LOG_FILE, "User accounts log file.")
+	flag.StringVar(&accountsFile, "file", DEFAULT_ACCOUNTS_FILE, "Specifies the name of the user accounts file.")
 	helpFlag = flag.Bool("help", false, "Help required by user.")
 	flag.Parse()
 
@@ -199,31 +313,60 @@ func handleCommandLineParameters() (int, error) {
 func showSyntax() {
 	simplelogging.LogMessage("Showing command syntax", simplelogging.LOG_INFO)
 
-	fmt.Printf("UASERVER [-HELP] | [-PORT = <port number>] [LOGFILE = <filepath>]\n\n")
-	fmt.Println("<port number> - HTTP port number for server listener. Defaults to 8080.")
-	fmt.Println("<filepath>    - Full or partial path of logfile. Defaults to LOGFILE.TXT in current folder.\n")
+	fmt.Printf("UASERVER [-HELP] | [-PORT = <port number>] [-FILE = <accounts file path>] [-LOGFILE = <log file path>]\n\n")
+	fmt.Println("<port number>        - HTTP port number for server listener. Defaults to 8080.")
+	fmt.Println("<accounts file path> - Full or partial path of accounts file. Defaults to USERS.DAT in current folder.\n")
+	fmt.Println("<log file path>      - Full or partial path of log file. Defaults to LOGFILE.TXT in current folder.\n")
 
 }
 
-// password encryption suppport
-func generateHashFromPassword (password []byte) (error, string) {
-    hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
-    if err != nil {
-        return err, ""
-    }
+//
+// (Hashed) Password Support
+//
+func generateHashFromPassword(password []byte) (error, string) {
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
+	if err != nil {
+		return err, ""
+	}
 
-    return nil, string(hash)  
+	return nil, string(hash)
 }
 
-func comparePasswordAgainstHash (hashedPassword string, plaintextPassword []byte) bool {
-    byteHash := []byte(hashedPassword)
-    err := bcrypt.CompareHashAndPassword(byteHash, plaintextPassword)
-    if err != nil {
-       return false
-    }
-    
-    return true
-} 
+func comparePasswordAgainstHash(hashedPassword string, plaintextPassword []byte) bool {
+	byteHash := []byte(hashedPassword)
+	err := bcrypt.CompareHashAndPassword(byteHash, plaintextPassword)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+//
+// Session/Token Support
+//
+//func setSession (userName string, response http.ResponseWriter) {
+//	value := map[string]string{"name": userName,}
+//	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+//		cookie := &http.Cookie{	Name:  "session",Value: encoded,Path:  "/",}
+//		http.SetCookie(response, cookie)
+//	}
+//}
+
+//func clearSession (response http.ResponseWriter) {
+//	cookie := &http.Cookie{Name:   "session",Value:  "",Path:   "/",MaxAge: -1,}
+//	http.SetCookie(response, cookie)
+//}
+
+//func GetSessionToken(request *http.Request) string {
+//	if cookie, err := request.Cookie("session"); err == nil {
+//		cookieValue := make(map[string]string)
+//		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+//			token = cookieValue["name"]
+//		}
+//	}
+//	return token
+//}
 
 func main() {
 
@@ -238,7 +381,7 @@ func main() {
 	}
 
 	if err == nil {
-		fmt.Printf("Starting UAServer on Port %d ...\n", httpPort)
+		fmt.Printf("Starting User Accounts Server %s on Port %d ...\n", SERVER_VERSION, httpPort)
 		handleRequests()
 	}
 }
